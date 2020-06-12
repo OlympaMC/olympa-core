ab0fee39217e6071566452b97cf3ba3be9d88544
Tristiisch <tristiisch74@hotmail.com>
2020-06-12 14:30:22 +0200

    test /startserv

--- src/java/fr/olympa/core/bungee/servers/ServerStartStop.java
+++ src/java/fr/olympa/core/bungee/servers/ServerStartStop.java
@@ -8,7 +8,7 @@ import fr.olympa.core.bungee.OlympaBungee;
 import net.md_5.bungee.api.CommandSender;
 
 public class ServerStartStop {
-
+	
 	@SuppressWarnings("deprecation")
 	public static void action(String action, String serverName, CommandSender sender) {
 		new Thread((Runnable) () -> {
@@ -18,20 +18,18 @@ public class ServerStartStop {
 				p = Runtime.getRuntime().exec("mc " + action + " " + serverName);
 				BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
 				while ((s = br.readLine()) != null) {
-					if (sender != null) {
+					if (sender != null)
 						sender.sendMessage("§c[§4OUT§c] §c" + s);
-					}
 					OlympaBungee.getInstance().sendMessage("§c[§4OUT§c] §c" + s);
 				}
 				p.waitFor();
-				if (sender != null) {
+				if (sender != null)
 					sender.sendMessage("§c[§4OUT§c] §c" + p.exitValue());
-				}
 				OlympaBungee.getInstance().sendMessage("§c[§4OUT§c] §c" + p.exitValue());
 				p.destroy();
 			} catch (IOException | InterruptedException e) {
 				e.printStackTrace();
 			}
-		});
+		}).start();
 	}
 }
