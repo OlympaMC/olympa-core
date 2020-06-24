b5ddaf269acccde94b894e954f87303afe821988
Tristiisch <tristiisch74@hotmail.com>
2020-06-24 11:47:19 +0200

    fix

--- src/java/fr/olympa/core/spigot/commands/RestartCommand.java
+++ src/java/fr/olympa/core/spigot/commands/RestartCommand.java
@@ -13,30 +13,30 @@ import fr.olympa.api.command.OlympaCommand;
 import fr.olympa.api.permission.OlympaCorePermissions;
 
 public class RestartCommand extends OlympaCommand {
-	
+
 	public RestartCommand(Plugin plugin) {
 		super(plugin, "restart", "Redémarre le serveur", OlympaCorePermissions.SERVER_RESTART_COMMAND);
 		allowConsole = true;
 	}
-
+	
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		restart();
 		return false;
 	}
-
+	
 	@Override
 	public List<String> onTabComplete(CommandSender sender, Command cmd, String label, String[] args) {
 		return null;
 	}
-
+	
 	public void restart() {
 		plugin.getServer().shutdown();
 		new Thread((Runnable) () -> {
 			try {
 				String s;
 				Process p;
-				p = Runtime.getRuntime().exec("sleep 5 && sh start.sh");
+				p = Runtime.getRuntime().exec("sh start.sh");
 				BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
 				StringBuilder sb = new StringBuilder();
 				while ((s = br.readLine()) != null)
