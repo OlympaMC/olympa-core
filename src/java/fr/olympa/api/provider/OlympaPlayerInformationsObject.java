c142bd62323ba56aae8fd89adc2ba32261b54a1b
Tristiisch <tristiisch74@hotmail.com>
2020-06-24 05:38:13 +0200

    fixs

--- src/java/fr/olympa/api/provider/OlympaPlayerInformationsObject.java
+++ src/java/fr/olympa/api/provider/OlympaPlayerInformationsObject.java
@@ -2,6 +2,7 @@ package fr.olympa.api.provider;
 
 import java.util.UUID;
 
+import fr.olympa.api.clans.ClanPlayerData;
 import fr.olympa.api.player.OlympaPlayerInformations;
 
 public class OlympaPlayerInformationsObject implements OlympaPlayerInformations {
@@ -9,6 +10,7 @@ public class OlympaPlayerInformationsObject implements OlympaPlayerInformations 
 	private final long id;
 	private final String pseudo;
 	private final UUID uuid;
+	private ClanPlayerData clanData;
 
 	public OlympaPlayerInformationsObject(long id, String pseudo, UUID uuid) {
 		this.id = id;
@@ -16,6 +18,10 @@ public class OlympaPlayerInformationsObject implements OlympaPlayerInformations 
 		this.uuid = uuid;
 	}
 
+	public void setClanData(ClanPlayerData clanData) {
+		this.clanData = clanData;
+	}
+	
 	@Override
 	public long getId() {
 		return id;
@@ -33,11 +39,16 @@ public class OlympaPlayerInformationsObject implements OlympaPlayerInformations 
 
 	@Override
 	public boolean equals(Object obj) {
-		if (obj == this) return true;
-		if (obj instanceof OlympaPlayerInformationsObject) {
+		if (obj == this)
+			return true;
+		if (obj instanceof OlympaPlayerInformationsObject)
 			return id == ((OlympaPlayerInformationsObject) obj).id;
-		}
 		return false;
+	}
+
+	@Override
+	public ClanPlayerData getClanData() {
+		return clanData;
 	}
 	
 }
--- src/java/fr/olympa/core/bungee/privatemessage/PrivateMessageToggleCommand.java
+++ src/java/fr/olympa/core/bungee/privatemessage/PrivateMessageToggleCommand.java
@@ -7,31 +7,30 @@ import java.util.UUID;
 import fr.olympa.api.permission.OlympaCorePermissions;
 import fr.olympa.api.utils.Prefix;
 import fr.olympa.core.bungee.api.command.BungeeCommand;
-import fr.olympa.core.bungee.utils.BungeeUtils;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Plugin;
 
 @SuppressWarnings("deprecation")
 public class PrivateMessageToggleCommand extends BungeeCommand {
-
+	
 	public static List<UUID> players = new ArrayList<>();
-
+	
 	public PrivateMessageToggleCommand(Plugin plugin) {
 		super(plugin, "msgtoggle", OlympaCorePermissions.PRIVATEMESSAGE_TOGGLE, "msgt");
 		allowConsole = false;
 	}
-
+	
 	@Override
 	public void onCommand(CommandSender sender, String[] args) {
 		ProxiedPlayer player = (ProxiedPlayer) sender;
 		if (players.contains(player.getUniqueId())) {
-			player.sendMessage(Prefix.DEFAULT_GOOD + BungeeUtils.color("Tes messages privés sont désormais &2&lactivés&2."));
+			sendMessage(Prefix.DEFAULT_GOOD, "Tes messages privés sont désormais &2&lactivés&2.");
 			players.remove(player.getUniqueId());
 		} else {
-			player.sendMessage(Prefix.DEFAULT_GOOD + BungeeUtils.color("Tes messages privés sont désormais &2&ldésactivés&2."));
+			sendMessage(Prefix.DEFAULT_GOOD, "Tes messages privés sont désormais &2&ldésactivés&2.");
 			players.add(player.getUniqueId());
 		}
 	}
-
+	
 }
