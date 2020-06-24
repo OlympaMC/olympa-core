c6c5278f91cbf1db00634df7ebd1e37f5cd0034a
Tristiisch <tristiisch74@hotmail.com>
2020-06-24 06:36:34 +0200

    fix color

--- src/java/fr/olympa/core/bungee/servers/ScriptBashMc.java
+++ src/java/fr/olympa/core/bungee/servers/ScriptBashMc.java
@@ -7,21 +7,33 @@ import java.io.InputStreamReader;
 import fr.olympa.api.utils.Prefix;
 import fr.olympa.core.bungee.api.command.BungeeCommand;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
 
 public class ScriptBashMc {
 
-	public static void action(String action, String serverName, CommandSender sender) {
+	public static void action(String action, String serverName, CommandSender commandSender) {
 		new Thread((Runnable) () -> {
+			CommandSender sender;
+			if (commandSender == null)
+				sender = ProxyServer.getInstance().getConsole();
+			else
+				sender = commandSender;
 			try {
 				Process p = Runtime.getRuntime().exec("mc " + action + " " + serverName);
-				if (sender != null) {
-					BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-					String line;
-					while ((line = br.readLine()) != null) {
-						BungeeCommand.sendMessage(sender, line.replace("[0m", "&f").replace("[32m", "&3").replace("[36m", "&b").replace("[36m", "&2").replace("[0;36m", "&a").replace("[31m", "&4").replace("[0;31m", "&c"));
-					}
-					br.close();
-				}
+				BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
+				String line;
+				while ((line = br.readLine()) != null)
+					BungeeCommand.sendMessage(sender,
+							line.replace("[]", "")
+									.replace("[0m", "&f")
+									.replace("[0;32m", "&3")
+									.replace("[32m", "&3")
+									.replace("[36m", "&b")
+									.replace("[36m", "&2")
+									.replace("[0;36m", "&a")
+									.replace("[31m", "&4")
+									.replace("[0;31m", "&c"));
+				br.close();
 				/*StringBuilder sb = new StringBuilder();
 				while ((s = br.readLine()) != null)
 					sb.append(s);
