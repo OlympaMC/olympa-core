00dbc5eb05f15639e8e304bee44c2f9431d1ee44
Tristiisch <tristiisch74@hotmail.com>
2020-06-29 06:39:36 +0200

    fix tabtext

--- src/java/fr/olympa/core/bungee/tabtext/TabText.java
+++ src/java/fr/olympa/core/bungee/tabtext/TabText.java
@@ -11,40 +11,40 @@ import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class TabText {
-	
+
 	public static void sendAll() {
 		send(ProxyServer.getInstance().getPlayers());
 	}
-	
+
 	public static void send(Collection<ProxiedPlayer> collection) {
 		String header = getHeader();
 		String footer = getFooter();
 		collection.forEach(p -> send(p, header, footer));
 	}
-
+	
 	public static void send(ProxiedPlayer player) {
 		String header = getHeader();
 		String footer = getFooter();
 		send(player, header, footer);
 	}
-
+	
 	public static void send(ProxiedPlayer player, String header, String footer) {
 		player.setTabHeader(TextComponent.fromLegacyText(header), TextComponent.fromLegacyText(footer));
 	}
-
+	
 	private static String getHeader() {
 		StringJoiner sj = new StringJoiner("\n");
 		sj.add("&e&lOlympa &eΩ");
 		sj.add("&6Versions 1.9 à 1.15");
 		return ColorUtils.color(sj.toString());
-		
-	}
 
+	}
+	
 	private static String getFooter() {
 		StringJoiner sj = new StringJoiner("\n");
 		//		sj.add("&6Serveur &e%n");
 		int onlineCount = OlympaBungee.getInstance().getProxy().getOnlineCount();
-		sj.add("&e%o &6joueur%os".replace("%o", String.valueOf(onlineCount).replace("%os", Utils.withOrWithoutS(onlineCount))));
+		sj.add("&e%o &6joueur%s".replace("%o", String.valueOf(onlineCount).replace("%s", Utils.withOrWithoutS(onlineCount))));
 		sj.add("");
 		sj.add("&6Discord &e&n&lwww.discord.olympa.fr &7| &6Teamspeak &e&n&lts.olympa.fr");
 		sj.add("");
