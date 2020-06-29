75fa24b964518e42685e8760d7e780ff8a7dce4d
Tristiisch <tristiisch74@hotmail.com>
2020-06-29 06:20:09 +0200

    up

--- src/java/fr/olympa/core/bungee/tabtext/TabText.java
+++ src/java/fr/olympa/core/bungee/tabtext/TabText.java
@@ -6,14 +6,14 @@ import java.util.StringJoiner;
 import fr.olympa.api.utils.ColorUtils;
 import fr.olympa.api.utils.Utils;
 import fr.olympa.core.bungee.OlympaBungee;
+import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class TabText {
 	
 	public static void sendAll() {
-		send(OlympaBungee.getInstance().getProxy().getPlayers());
+		send(ProxyServer.getInstance().getPlayers());
 	}
 	
 	public static void send(Collection<ProxiedPlayer> collection) {
@@ -29,12 +29,7 @@ public class TabText {
 	}
 
 	public static void send(ProxiedPlayer player, String header, String footer) {
-		ServerInfo serverInfo = player.getServer().getInfo();
-		int connected = serverInfo.getPlayers().size();
-		player.setTabHeader(TextComponent.fromLegacyText(header), TextComponent.fromLegacyText(footer
-				.replace("%n", serverInfo.getName())
-				.replace("%o", String.valueOf(connected))
-				.replace("%os", Utils.withOrWithoutS(connected))));
+		player.setTabHeader(TextComponent.fromLegacyText(header), TextComponent.fromLegacyText(footer));
 	}
 
 	private static String getHeader() {
@@ -47,11 +42,12 @@ public class TabText {
 
 	private static String getFooter() {
 		StringJoiner sj = new StringJoiner("\n");
-		sj.add("&6Serveur &e%n");
+		//		sj.add("&6Serveur &e%n");
 		int onlineCount = OlympaBungee.getInstance().getProxy().getOnlineCount();
-		sj.add("&e%o &6joueur%os sur &e" + onlineCount);
+		sj.add("&e%o &6joueur%os".replace("%o", String.valueOf(onlineCount).replace("%os", Utils.withOrWithoutS(onlineCount))));
 		sj.add("");
 		sj.add("&6Discord &e&n&lwww.discord.olympa.fr &7| &6Teamspeak &e&n&lts.olympa.fr");
+		sj.add("");
 		sj.add("&6Twitter &e&l@Olympa_fr &7| &6Site &e&n&lwww.olympa.fr");
 		return ColorUtils.color(sj.toString());
 	}
--- src/java/fr/olympa/core/bungee/tabtext/TabTextListener.java
+++ src/java/fr/olympa/core/bungee/tabtext/TabTextListener.java
@@ -1,7 +1,7 @@
 package fr.olympa.core.bungee.tabtext;
 
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
-import net.md_5.bungee.api.event.ServerConnectedEvent;
+import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
@@ -19,7 +19,7 @@ public class TabTextListener implements Listener {
 	}
 
 	@EventHandler
-	public void onPostLogin(ServerConnectedEvent event) {
+	public void onServerConnected(PostLoginEvent event) {
 		//		long t = Utils.getCurrentTimeInSeconds();
 		//		if (t - time < 10) {
 		//		event.getPlayer().setTabHeader(header, footer);
@@ -27,6 +27,7 @@ public class TabTextListener implements Listener {
 		//		}
 		//		time = t;
 		TabText.sendAll();
+		//		TabText.send(event.getPlayer());
 	}
 
 }
