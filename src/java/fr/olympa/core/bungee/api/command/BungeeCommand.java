b65867d8ed724e62377b913f3a7f87d6404bd071
Tristiisch <tristiisch74@hotmail.com>
2020-06-30 10:11:28 +0200

    ajouts du support d'aliases dans BungeeCommand#registerPreProcess()

--- src/java/fr/olympa/core/bungee/api/command/BungeeCommand.java
+++ src/java/fr/olympa/core/bungee/api/command/BungeeCommand.java
@@ -1,7 +1,9 @@
 package fr.olympa.core.bungee.api.command;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import fr.olympa.api.permission.OlympaPermission;
@@ -18,9 +20,9 @@ import net.md_5.bungee.api.plugin.Command;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public abstract class BungeeCommand extends Command {
-	
-	static Map<String, BungeeCommand> commandPreProcess = new HashMap<>();
 
+	static Map<List<String>, BungeeCommand> commandPreProcess = new HashMap<>();
+	
 	protected String[] aliases;
 	public boolean allowConsole = true;
 	protected String command;
@@ -28,29 +30,29 @@ public abstract class BungeeCommand extends Command {
 	protected boolean bypassAuth = false;
 	public OlympaPlayer olympaPlayer;
 	protected OlympaPermission permission;
-	
+
 	/**
 	 * Don't foget to set {@link BungeeCommand#usageString}
 	 */
 	public Integer minArg = 0;
-	
+
 	protected Plugin plugin;
 	public ProxiedPlayer proxiedPlayer;
 	private CommandSender sender;
-	
+
 	/**
 	 * Format: Usage » %command% <%obligatory%|%obligatory%> [%optional%] Variable
 	 * name: 'joueur' ...
 	 *
 	 */
 	public String usageString;
-	
+
 	public BungeeCommand(Plugin plugin, String command) {
 		super(command);
 		this.plugin = plugin;
 		this.command = command;
 	}
-	
+
 	public BungeeCommand(Plugin plugin, String command, OlympaPermission permission, String... aliases) {
 		super(command, null, aliases);
 		this.plugin = plugin;
@@ -58,7 +60,7 @@ public abstract class BungeeCommand extends Command {
 		this.permission = permission;
 		this.aliases = aliases;
 	}
-	
+
 	public BungeeCommand(Plugin plugin, String command, OlympaPermission permission, String[] aliases, String description, String usageString, boolean allowConsole, Integer minArg) {
 		super(command, null, aliases);
 		this.plugin = plugin;
@@ -71,18 +73,18 @@ public abstract class BungeeCommand extends Command {
 		this.minArg = minArg;
 		register();
 	}
-	
+
 	public BungeeCommand(Plugin plugin, String command, String... aliases) {
 		super(command, null, aliases);
 		this.plugin = plugin;
 		this.command = command;
 		this.aliases = aliases;
 	}
-	
+
 	public String buildText(int min, String[] args) {
 		return String.join(" ", Arrays.copyOfRange(args, min, args.length));
 	}
-	
+
 	@Override
 	public void execute(CommandSender sender, String[] args) {
 		ProxyServer.getInstance().getScheduler().runAsync(plugin, () -> {
@@ -93,7 +95,7 @@ public abstract class BungeeCommand extends Command {
 					sendError("Tu dois être connecté. Fait &4/login <mdp>&c.");
 					return;
 				}
-				
+
 				if (permission != null) {
 					olympaPlayer = getOlympaPlayer();
 					if (olympaPlayer == null) {
@@ -109,86 +111,89 @@ public abstract class BungeeCommand extends Command {
 				sendImpossibleWithConsole();
 				return;
 			}
-			
+
 			if (args.length < minArg) {
 				sendUsage();
 				return;
 			}
 			onCommand(sender, args);
 		});
-		
+
 	}
-	
+
 	public String getCommand() {
 		return command;
 	}
-	
+
 	protected OlympaPlayer getOlympaPlayer() {
 		if (proxiedPlayer == null)
 			return null;
 		return olympaPlayer == null ? olympaPlayer = new AccountProvider(proxiedPlayer.getUniqueId()).getFromRedis() : olympaPlayer;
 	}
-	
+
 	public OlympaPermission getPerm() {
 		return permission;
 	}
-	
+
 	public ProxiedPlayer getProxiedPlayer() {
 		return proxiedPlayer;
 	}
-	
-	public abstract void onCommand(CommandSender sender, String[] args);
 
+	public abstract void onCommand(CommandSender sender, String[] args);
+	
 	public void register() {
 		plugin.getProxy().getPluginManager().registerCommand(plugin, this);
 	}
-	
+
 	public BungeeCommand registerPreProcess() {
-		commandPreProcess.put(command, this);
+		List<String> commands = new ArrayList<>();
+		commands.add(command);
+		commands.addAll(Arrays.asList(aliases));
+		commandPreProcess.put(commands, this);
 		return this;
 	}
-	
+
 	public void sendDoNotHavePermission() {
 		sendError("Tu as pas la permission &l(◑_◑)");
 	}
-	
+
 	public void sendError(String message) {
 		this.sendMessage(Prefix.DEFAULT_BAD, message);
 	}
-	
+
 	public void sendImpossibleWithConsole() {
 		sendError("Impossible avec la console.");
 	}
-	
+
 	public void sendImpossibleWithOlympaPlayer() {
 		sendError("Une erreur est survenu avec tes donnés.");
 	}
-	
+
 	public static void sendMessage(CommandSender sender, Prefix prefix, String text) {
 		sendMessage(sender, prefix + BungeeUtils.color(text));
 	}
-	
+
 	public static void sendMessage(CommandSender sender, String text) {
 		sender.sendMessage(TextComponent.fromLegacyText(BungeeUtils.color(text)));
 	}
-	
+
 	public void sendMessage(Prefix prefix, String text) {
 		sendMessage(sender, prefix, text);
 	}
-	
+
 	public void sendMessage(String text) {
 		sendMessage(sender, BungeeUtils.color(text));
 	}
-	
+
 	public void sendMessage(TextComponent text) {
 		proxiedPlayer.sendMessage(text);
 	}
-	
+
 	public void sendUnknownPlayer(String name) {
 		sendError("Le joueur &4" + name + "&c est introuvable.");
 		// TODO check historique player
 	}
-	
+
 	public void sendUsage() {
 		this.sendMessage(Prefix.USAGE, "/" + command + " " + usageString);
 	}
--- src/java/fr/olympa/core/bungee/api/command/BungeeCommandListener.java
+++ src/java/fr/olympa/core/bungee/api/command/BungeeCommandListener.java
@@ -1,5 +1,7 @@
 package fr.olympa.core.bungee.api.command;
 
+import java.util.Arrays;
+
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ChatEvent;
 import net.md_5.bungee.api.plugin.Listener;
@@ -7,7 +9,7 @@ import net.md_5.bungee.event.EventHandler;
 import net.md_5.bungee.event.EventPriority;
 
 public class BungeeCommandListener implements Listener {
-	
+
 	@EventHandler(priority = EventPriority.LOWEST)
 	public void onChat(ChatEvent event) {
 		String message = event.getMessage();
@@ -19,10 +21,10 @@ public class BungeeCommandListener implements Listener {
 		BungeeCommand command = BungeeCommand.commandPreProcess.get(args[0]);
 		if (command == null)
 			return;
-
+		
 		ProxiedPlayer player = (ProxiedPlayer) event.getSender();
-		command.execute(player, args);
+		command.execute(player, Arrays.copyOfRange(args, 1, args.length));
 		event.setCancelled(true);
 	}
-	
+
 }
