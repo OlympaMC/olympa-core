4f750a1d9c9d909bffbc10d6c94f1617fe5a5f44
Tristiisch <tristiisch74@hotmail.com>
2020-06-30 03:37:25 +0200

    fix tab

--- src/java/fr/olympa/core/bungee/tabtext/TabText.java
+++ src/java/fr/olympa/core/bungee/tabtext/TabText.java
@@ -17,8 +17,6 @@ public class TabText {
 	}
 	
 	public static void send(Collection<ProxiedPlayer> collection) {
-		if (collection.isEmpty())
-			return;
 		String header = getHeader();
 		String footer = getFooter();
 		collection.forEach(p -> send(p, header, footer));
@@ -46,7 +44,7 @@ public class TabText {
 		StringJoiner sj = new StringJoiner("\n");
 		//		sj.add("&6Serveur &e%n");
 		int onlineCount = OlympaBungee.getInstance().getProxy().getOnlineCount();
-		sj.add("&e%o &6joueur%s".replace("%o", String.valueOf(onlineCount).replace("%s", Utils.withOrWithoutS(onlineCount))));
+		sj.add("&e%o &6joueur%s".replace("%o", String.valueOf(onlineCount)).replace("%s", Utils.withOrWithoutS(onlineCount)));
 		sj.add("");
 		sj.add("&6Discord &e&n&lwww.discord.olympa.fr &7| &6Teamspeak &e&n&lts.olympa.fr");
 		sj.add("");
