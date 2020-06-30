691397cf1b6e66b8236d1f0fd5e470ec8d265e45
Tristiisch <tristiisch74@hotmail.com>
2020-06-30 14:16:47 +0200

    retrait des messages "staff" quand un staff ne s'est pas login

--- src/java/fr/olympa/core/bungee/staffchat/StaffChatHandler.java
+++ src/java/fr/olympa/core/bungee/staffchat/StaffChatHandler.java
@@ -9,6 +9,7 @@ import fr.olympa.api.player.OlympaPlayer;
 import fr.olympa.api.provider.AccountProvider;
 import fr.olympa.api.utils.Prefix;
 import fr.olympa.api.utils.Utils;
+import fr.olympa.core.bungee.datamanagment.DataHandler;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.BaseComponent;
@@ -16,18 +17,18 @@ import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class StaffChatHandler {
-
+	
 	private static final Set<UUID> staffChat = new HashSet<>();
-
+	
 	public static void sendMessage(OlympaPlayer olympaPlayer, CommandSender sender, String msg) {
 		String message = msg.replaceAll("( )\\1+", " ");
 		BaseComponent[] messageComponent = TextComponent.fromLegacyText(Prefix.STAFFCHAT +
 				(sender instanceof ProxiedPlayer ? Utils.capitalize(((ProxiedPlayer) sender).getServer().getInfo().getName()) + " " + olympaPlayer.getGroupNameColored() + " " : "§e") + "§l" + sender.getName() + " §7: " + message);
-		ProxyServer.getInstance().getPlayers().stream().filter(p -> OlympaCorePermissions.STAFF_CHAT.hasPermission(new AccountProvider(p.getUniqueId()).getFromRedis()))
+		ProxyServer.getInstance().getPlayers().stream().filter(p -> !DataHandler.isUnlogged(p) && OlympaCorePermissions.STAFF_CHAT.hasPermission(new AccountProvider(p.getUniqueId()).getFromRedis()))
 				.forEach(p -> p.sendMessage(messageComponent));
 		ProxyServer.getInstance().getConsole().sendMessage(messageComponent);
 	}
-	
+
 	public static Set<UUID> getStaffchat() {
 		return staffChat;
 	}
--- src/java/fr/olympa/core/bungee/utils/BungeeUtils.java
+++ src/java/fr/olympa/core/bungee/utils/BungeeUtils.java
@@ -19,20 +19,20 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.config.Configuration;
 
 public class BungeeUtils {
-
+	
 	public static List<String> color(List<String> l) {
 		return l.stream().map(s -> BungeeUtils.color(s)).collect(Collectors.toList());
 	}
-
+	
 	public static String color(String s) {
 		return s != null ? ChatColor.translateAlternateColorCodes('&', s) : "";
 	}
-
+	
 	public static String connectScreen(String s) {
 		Configuration config = OlympaBungee.getInstance().getConfig();
 		return BungeeUtils.color(config.getString("default.connectscreenprefix") + s + config.getString("default.connectscreensuffix"));
 	}
-
+	
 	public static TextComponent formatStringToJSON(String s) {
 		TextComponent textcomponent = new TextComponent();
 		BaseComponent[] msgs = TextComponent.fromLegacyText(BungeeUtils.color(s));
@@ -40,21 +40,21 @@ public class BungeeUtils {
 			textcomponent.addExtra(msg);
 		return textcomponent;
 	}
-
+	
 	public static String getName(UUID uuid) {
 		return null;
 	}
-
+	
 	public static Set<ProxiedPlayer> getPlayers(List<String> l) {
 		return OlympaBungee.getInstance().getProxy().getPlayers().stream().filter(p -> !DataHandler.isUnlogged(p)).collect(Collectors.toSet());
 	}
-
+	
 	public static void getPlayers(OlympaPermission permission, Consumer<? super Set<ProxiedPlayer>> success, Consumer<? super Set<ProxiedPlayer>> noPerm) {
 		Set<ProxiedPlayer> playersWithNoPerm = new HashSet<>();
 		Set<ProxiedPlayer> playersWithPerm = new HashSet<>();
 		OlympaBungee.getInstance().getProxy().getPlayers().stream().forEach(p -> {
 			OlympaPlayer op = new AccountProvider(p.getUniqueId()).getFromRedis();
-			if (op != null && permission.hasPermission(op))
+			if (op != null && !DataHandler.isUnlogged(p) && permission.hasPermission(op))
 				playersWithPerm.add(p);
 			else
 				playersWithNoPerm.add(p);
