0dc0bcd538d32a7095932848e831d61376c9eb3a
Tristiisch <tristiisch74@hotmail.com>
2021-02-22 03:48:56 +0100

    up deploy files

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-(cd /home/repo/olympaapi/ && sh ./deploy.sh) && git pull && mvn install && cp target/Olympa*.jar /home/serveurs/$1/plugins/ && mc restart $1
+(cd /home/repo/olympaapi/ && sh ./build.sh) && git pull && mvn install
