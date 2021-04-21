17777379b25dfb849713dd138e54d8390a37ec94
Tristiisch <tristiisch74@hotmail.com>
2021-04-21 19:05:05 +0200

    up script

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@
 
 (cd /home/repo/olympaapi/ && sh ./deploy.sh $1)
 if [ "$?" -ne 0 ]; then
-	echo "\e[91mArrêt de la création des JAR. Erreur code $rc\e[0m"; exit $rc
+	echo -e "\e[91mArrêt de la création des JAR. Erreur code $rc\e[0m"; exit $rc
 fi
 
 USE_BRANCH="master dev"
@@ -33,7 +33,7 @@ if [ -n "$1" ]; then
 		SERV="$2"
 	fi
 else
-	echo "\e[0;36mTu peux choisir la version du core en ajoutant une date (ex './deploy.sh date \"2021-02-26 18:30:00\"') ou une branch (ex './deploy.sh dev').\e[0m"
+	echo -e  "\e[0;36mTu peux choisir la version du core en ajoutant une date (ex './deploy.sh date \"2021-02-26 18:30:00\"') ou une branch (ex './deploy.sh dev').\e[0m"
 fi
 
 if [ -n "$BRANCH_NAME" ]; then
@@ -62,10 +62,10 @@ if [ -n "$ACTUAL_COMMIT_ID" ] && [ -n "$ACTUAL_COMMIT_ID_API" ]; then
 	git pull
 	if [ "$ACTUAL_COMMIT_ID" = `git rev-parse HEAD` ]; then
 		if [ "$ACTUAL_COMMIT_ID_API" = `cd ../olympaapi && git rev-parse HEAD` ]; then
-			echo "\e[32mPas besoin de maven install le core, l'api est up & le jar est déjà crée.\e[0m"
+			echo -e  "\e[32mPas besoin de maven install le core, l'api est up & le jar est déjà crée.\e[0m"
 			exit 0
 		else
-			echo "\e[32mIl faut build le core, l'api a été up.\e[0m"
+			echo -e  "\e[32mIl faut build le core, l'api a été up.\e[0m"
 		fi
 	fi
 fi
@@ -76,5 +76,5 @@ else
 	echo `git rev-parse HEAD` > target/commitId
 	echo `cd ../olympaapi && git rev-parse HEAD` > target/commitIdAPI
 fi
-echo "\e[32mLe jar du commit de l'api $(cat target/commitIdAPI) avec le core $(cat target/commitId) a été crée.\e[0m"
+echo -e "\e[32mLe jar du commit de l'api $(cat target/commitIdAPI) avec le core $(cat target/commitId) a été crée.\e[0m"
 exit 0
