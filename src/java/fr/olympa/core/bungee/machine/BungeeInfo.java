d51f6200d088e45bc227eef052c0bb40365d14e7
Tristiisch <tristiisch74@hotmail.com>
2021-05-12 22:14:06 +0200

    fix /tps

--- src/java/fr/olympa/core/bungee/machine/BungeeInfo.java
+++ src/java/fr/olympa/core/bungee/machine/BungeeInfo.java
@@ -20,7 +20,7 @@ public class BungeeInfo extends MachineMessage {
 			throw new UnsupportedOperationException("Unable to get Bungee Info on not Bungee Environment");
 		TxtComponentBuilder textBuilder = super.getInfoMessage();
 		try {
-			textBuilder.extra(new TxtComponentBuilder("&3Plugins Olympa: &b"));
+			textBuilder.extra(new TxtComponentBuilder("\n&3Plugins Olympa: &b"));
 			for (TxtComponentBuilder txt : ((OlympaBungee) main).getProxy().getPluginManager().getPlugins().stream().filter(f -> f.getDescription().getName().startsWith("Olympa"))
 					.map(ff -> {
 						String fileInfo = Utils.tsToShortDur(new File(ff.getClass().getProtectionDomain().getCodeSource().getLocation().getPath()).lastModified() / 1000L);
@@ -28,7 +28,6 @@ public class BungeeInfo extends MachineMessage {
 					})
 					.collect(Collectors.toList()))
 				textBuilder.extra(txt);
-			textBuilder.extra("\n");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
