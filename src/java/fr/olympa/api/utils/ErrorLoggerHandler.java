99881506faf2cf0ebdb4467026f2eab543dfeb2f
SkytAsul <skytasul@gmail.com>
2021-05-14 22:21:23 +0200

    Up error handler

--- src/java/fr/olympa/api/utils/ErrorLoggerHandler.java
+++ src/java/fr/olympa/api/utils/ErrorLoggerHandler.java
@@ -21,9 +21,9 @@ public class ErrorLoggerHandler extends Handler {
 		if (record.getThrown() != null) {
 			String stackTrace = ExceptionUtils.getStackTrace(record.getThrown());
 			try {
-				sendError.accept(record.getLevel().getName() + " [" + record.getLoggerName() + "] " + record.getMessage().replaceAll("executing task \\d+", "executing task XXX").replaceAll("Task \\d+ for", "Task XXX for") + "\n" + stackTrace);
+				sendError.accept(record.getLevel().getName() + " [" + record.getLoggerName() + "] " + record.getMessage().replaceAll("ask #?\\d+", "ask XXX") + "\n" + stackTrace); // remove "Task XXXX"
 			}catch (Exception ex) {
-				OlympaCore.getInstance().sendMessage("§cUne erreur est survenue durant le passage d'une erreur au bungee via redis:\n%s", stackTrace);
+				OlympaCore.getInstance().sendMessage("§cUne erreur est survenue durant le passage d'une erreur au bungee via redis: ", ExceptionUtils.getMessage(ex));
 			}
 		}
 	}
