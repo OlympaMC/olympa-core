9c766789f642db004967b7fadfe36fddc3358a37
Tristiisch <tristiisch74@hotmail.com>
2021-05-17 13:20:29 +0200

    Compacte ./help

--- src/java/fr/olympa/core/spigot/security/HelpCommand.java
+++ src/java/fr/olympa/core/spigot/security/HelpCommand.java
@@ -7,36 +7,36 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
+import fr.olympa.api.chat.TxtComponentBuilder;
 import fr.olympa.api.command.OlympaCommand;
 import fr.olympa.api.command.Paginator;
 import fr.olympa.api.player.OlympaPlayer;
+import fr.olympa.api.utils.Prefix;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
-import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.chat.hover.content.Text;
 
 public class HelpCommand extends OlympaCommand {
 
 	private Paginator<OlympaCommand> paginator;
-	
+
 	public HelpCommand(Plugin plugin) {
 		super(plugin, "help", "?");
 		super.description = "Affiche l'aide.";
-		paginator = new Paginator<OlympaCommand>(10, "Aide §lOlympa") {
-			
+		paginator = new Paginator<>(10, "Aide §lOlympa") {
+
 			@Override
 			protected List<OlympaCommand> getObjects() {
 				return OlympaCommand.commands;
 			}
-			
+
 			@Override
 			protected BaseComponent getObjectDescription(OlympaCommand object) {
-				TextComponent commandCompo = new TextComponent(TextComponent.fromLegacyText("§7➤ §6/" + object.getCommand() + (object.getDescription() == null ? "" : ": §e" + object.getDescription())));
-				commandCompo.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText("§bClique pour afficher l'aide de cette commande !")));
-				commandCompo.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/help " + object.getCommand()));
-				return commandCompo;
+				return TxtComponentBuilder.of(Prefix.NONE, "§7➤ §6/" + object.getCommand() + (object.getDescription() == null ? "" : ": §e" + object.getDescription()),
+						ClickEvent.Action.RUN_COMMAND, "/help " + object.getCommand(), HoverEvent.Action.SHOW_TEXT, new Text("§bClique pour afficher l'aide de cette commande !"));
 			}
-			
+
 			@Override
 			protected String getCommand(int page) {
 				return "/help " + page;
@@ -44,36 +44,32 @@ public class HelpCommand extends OlympaCommand {
 		};
 	}
 
-	@SuppressWarnings("deprecation")
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		OlympaPlayer olympaPlayer = null;
 		if (player != null)
 			olympaPlayer = getOlympaPlayer();
 		int page = 1;
-		if (args.length > 0) {
+		if (args.length > 0)
 			try {
 				page = Integer.parseInt(args[0]);
 				if (page < 1) {
 					sendError("La page %d n'existe pas.", page);
 					return false;
 				}
-			}catch (NumberFormatException e) {
-				for (OlympaCommand command : OlympaCommand.commands) {
+			} catch (NumberFormatException e) {
+				for (OlympaCommand command : OlympaCommand.commands)
 					if (command.getAllCommands().contains(args[0])) {
-						if (player == null || command.hasPermission(olympaPlayer)) {
+						if (player == null || command.hasPermission(olympaPlayer))
 							command.sendHelp(sender);
-						}else {
+						else
 							sendError("Tu n'as pas la permission pour afficher l'aide de cette commande.");
-						}
 						return false;
 					}
-				}
 				sendError("La commande %s n'existe pas.", args[0]);
 				return false;
 			}
-		}
-		
+
 		sendComponents(paginator.getPage(page));
 		return false;
 	}
