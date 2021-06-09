88f29df83de5dba814105e006f821e8617ec88c9
Tristiisch <tristiisch74@hotmail.com>
2021-06-09 23:09:25 +0200

    fix /credit

--- src/java/fr/olympa/core/bungee/commands/CreditCommand.java
+++ src/java/fr/olympa/core/bungee/commands/CreditCommand.java
@@ -1,32 +1,30 @@
 package fr.olympa.core.bungee.commands;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import fr.olympa.api.bungee.command.BungeeComplexCommand;
+import fr.olympa.api.bungee.command.BungeeCommand;
+import fr.olympa.api.bungee.permission.OlympaBungeePermission;
 import fr.olympa.api.common.chat.TxtComponentBuilder;
-import fr.olympa.api.common.command.complex.Cmd;
-import fr.olympa.api.common.command.complex.CommandContext;
 import fr.olympa.api.common.plugin.PluginInfoAdvanced;
 import fr.olympa.core.bungee.servers.MonitorInfoBungee;
 import fr.olympa.core.bungee.servers.MonitorServers;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Plugin;
 
-public class CreditCommand extends BungeeComplexCommand {
+public class CreditCommand extends BungeeCommand {
 
 	public CreditCommand(Plugin plugin) {
-		super(plugin, "credit", "Affiche tous les devs ayant participer au serveur", null, "author", "auteur");
+		super(plugin, "credit", "Affiche tous les devs ayant participer au serveur", (OlympaBungeePermission) null, "author", "auteur");
 		minArg = 0;
 	}
 
-	@Cmd(otherArg = true, min = 0)
-	public void otherArg(CommandContext cmd) {
+	@Override
+	public void onCommand(CommandSender sender, String[] args) {
 		TxtComponentBuilder out = new TxtComponentBuilder("&eListe non exhaustive des développeurs de nos plugins > ").extraSpliterBN();
 		Collection<MonitorInfoBungee> allServers = MonitorServers.getServers();
-		List<String> serversNames = allServers.stream().map(mib -> mib.getOlympaServer().getNameCaps()).distinct().collect(Collectors.toList());
+		List<String> serversNames = allServers.stream().filter(mib -> mib.getServerDebugInfo() != null).map(mib -> mib.getOlympaServer().getNameCaps()).distinct().collect(Collectors.toList());
 		serversNames.forEach(name -> {
 			List<MonitorInfoBungee> servers = allServers.stream().filter(mib -> mib.getServerDebugInfo() != null
 					&& mib.getServerDebugInfo().getPlugins() != null && !mib.getServerDebugInfo().getPlugins().isEmpty()
@@ -38,10 +36,5 @@ public class CreditCommand extends BungeeComplexCommand {
 			out.extra(new TxtComponentBuilder("&6" + name + "\n&2Devs Olympa &a" + authorPluginOlympa + "\n&7Devs Plugin Public " + otherAuthors));
 		});
 		sendMessage(out.build());
-	}
-
-	@Override
-	public Iterable<String> onTabComplete(CommandSender sender, String[] args) {
-		return new ArrayList<>();
 	}
 }
