83c8b5cbcfdf5fe41c3e8eb63ec60c6eab336131
Tristiisch <tristiisch74@hotmail.com>
2021-06-25 11:11:25 +0200

    fix /tps

--- src/java/fr/olympa/core/spigot/commands/TpsCommand.java
+++ src/java/fr/olympa/core/spigot/commands/TpsCommand.java
@@ -41,7 +41,7 @@ public class TpsCommand extends OlympaCommand implements Listener, ModuleApi<Oly
 			else
 				sendMessage(Prefix.DEFAULT_BAD, "La TPS ActionBar a été désactivée.");
 		} else
-			sendComponents(new TpsMessage(getOlympaPlayer()).getInfoMessage().build());
+			sendComponents(new TpsMessage(player != null ? getOlympaPlayer() : null).getInfoMessage().build());
 		return false;
 	}
 
