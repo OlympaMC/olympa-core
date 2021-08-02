58f9193758e21782341277e0b46e1bf005ab9a75
SkytAsul <skytasul@gmail.com>
2021-08-02 15:12:55 +0200

    Update forcekick

--- src/java/fr/olympa/core/bungee/ban/commands/ForceKickCommand.java
+++ src/java/fr/olympa/core/bungee/ban/commands/ForceKickCommand.java
@@ -1,5 +1,8 @@
 package fr.olympa.core.bungee.ban.commands;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import fr.olympa.api.bungee.command.BungeeCommand;
 import fr.olympa.api.common.match.RegexMatcher;
 import fr.olympa.api.utils.Prefix;
@@ -7,6 +10,7 @@ import fr.olympa.core.bungee.OlympaBungee;
 import fr.olympa.core.common.permission.list.OlympaCorePermissionsBungee;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.PendingConnection;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 
 public class ForceKickCommand extends BungeeCommand {
@@ -25,8 +29,29 @@ public class ForceKickCommand extends BungeeCommand {
 				this.sendMessage(Prefix.DEFAULT_BAD + "Le joueur &4" + args[0] + "&c n'est pas connecté.");
 				return;
 			}
-			target.getPendingConnection().disconnect();
+			boolean force = args.length > 1 && Boolean.parseBoolean(args[1]);
+			PendingConnection connection = target.getPendingConnection();
+			try {
+				Field channelField = connection.getClass().getField("ch");
+				channelField.setAccessible(true);
+				Object channel = channelField.get(connection);
+				Method isClosing = channel.getClass().getDeclaredMethod("isClosing");
+				Method isClosed = channel.getClass().getDeclaredMethod("isClosed");
+				this.sendInfo("Connection is closing: %s, is closed: %s", isClosing.invoke(channel), isClosed.invoke(channel));
+			}catch (ReflectiveOperationException ex) {
+				ex.printStackTrace();
+			}
+			connection.disconnect();
 			this.sendMessage(Prefix.DEFAULT_GOOD + "Le joueur &2" + target.getName() + "&a a été kick.");
+			if (force) {
+				try {
+					Method removeConnection = ProxyServer.getInstance().getClass().getDeclaredMethod("removeConnection", connection.getClass());
+					removeConnection.invoke(ProxyServer.getInstance(), connection);
+					sendSuccess("Le joueur §2%s§a a été force-kick, sa connection est retirée.");
+				}catch (ReflectiveOperationException ex) {
+					ex.printStackTrace();
+				}
+			}
 
 		} else
 			sendUsage(command);
