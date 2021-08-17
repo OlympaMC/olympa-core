9d21c11ed7759751cb24e18adaa9a28ace30884a
SkytAsul <skytasul@gmail.com>
2021-08-17 13:38:08 +0200

    Up /restart to use new OlympaCore#restartServer

--- src/java/fr/olympa/core/spigot/commands/RestartCommand.java
+++ src/java/fr/olympa/core/spigot/commands/RestartCommand.java
@@ -1,14 +1,11 @@
 package fr.olympa.core.spigot.commands;
 
 import java.util.List;
-import java.util.function.Consumer;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.Plugin;
 
-import fr.olympa.api.common.bash.OlympaRuntime;
 import fr.olympa.api.spigot.command.OlympaCommand;
 import fr.olympa.core.common.permission.list.OlympaCorePermissionsSpigot;
 import fr.olympa.core.spigot.OlympaCore;
@@ -22,10 +19,7 @@ public class RestartCommand extends OlympaCommand {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		Bukkit.getOnlinePlayers().forEach(p -> p.kickPlayer("Server is restarting"));
-		Consumer<String> function = player != null ? out -> sender.sendMessage(out) : null;
-		Runtime.getRuntime().addShutdownHook(OlympaRuntime.action("sh start.sh", function));
-		((OlympaCore) plugin).getTask().runTaskLater(() -> plugin.getServer().shutdown(), 4 * 20);
+		OlympaCore.getInstance().restartServer(sender);
 		return false;
 	}
 
