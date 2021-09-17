eab1ead6179c21d1efc75ebfd6d08f6ee21e6997
Tristiisch <tristiisch74@hotmail.com>
2021-09-17 03:15:22 +0200

    fix /btps plugin details

--- src/java/fr/olympa/core/bungee/commands/BungeeLagCommand.java
+++ src/java/fr/olympa/core/bungee/commands/BungeeLagCommand.java
@@ -2,6 +2,7 @@ package fr.olympa.core.bungee.commands;
 
 import fr.olympa.api.bungee.command.BungeeCommand;
 import fr.olympa.api.common.machine.TpsMessageProvider;
+import fr.olympa.api.common.player.OlympaPlayer;
 import fr.olympa.core.common.permission.list.OlympaCorePermissionsBungee;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.plugin.Plugin;
@@ -14,7 +15,7 @@ public class BungeeLagCommand extends BungeeCommand {
 
 	@Override
 	public void onCommand(CommandSender sender, String[] args) {
-		sender.sendMessage(new TpsMessageProvider(getOlympaPlayer()).getInfoMessage().build());
+		sender.sendMessage(new TpsMessageProvider(super.<OlympaPlayer>getOlympaPlayerNullable()).getInfoMessage().build());
 	}
 
 }
